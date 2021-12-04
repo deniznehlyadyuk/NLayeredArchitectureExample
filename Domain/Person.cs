88f93457d78f3018180d47379d6d90cf98dd713c
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-04 18:46:28 +0300

    Person entitysi oluşturuldu.

---
+++ Domain/Person.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Core.Domain;
+
+namespace Domain
+{
+    public class Person : BaseEntity
+    {
+        [MaxLength(128)]
+        [Required]
+        public string FullName { get; set; }
+        
+        [MaxLength(11)]
+        [Required]
+        public string IdentityNumber { get; set; }
+        
+        [Required]
+        public DateTime BornDate { get; set; }
+    }
+}
