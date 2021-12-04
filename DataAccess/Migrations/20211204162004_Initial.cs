816d7602f5a5f8159295c12c69958d9cf5142446
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-04 19:22:00 +0300

    Initial migration'ı alındı.

---
+++ DataAccess/Migrations/20211204162004_Initial.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "lessons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(48)", maxLength: 48, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lessons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "persons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FullName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    IdentityNumber = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    BornDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SchoolNumber = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_students_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "teachers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teachers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_teachers_persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "student_scores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LessonId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExamNumber = table.Column<int>(type: "integer", nullable: false),
+                    Score = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_student_scores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_student_scores_lessons_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "lessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_student_scores_students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_student_scores_LessonId",
+                table: "student_scores",
+                column: "LessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_student_scores_StudentId",
+                table: "student_scores",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_students_PersonId",
+                table: "students",
+                column: "PersonId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_teachers_PersonId",
+                table: "teachers",
+                column: "PersonId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "student_scores");
+
+            migrationBuilder.DropTable(
+                name: "teachers");
+
+            migrationBuilder.DropTable(
+                name: "lessons");
+
+            migrationBuilder.DropTable(
+                name: "students");
+
+            migrationBuilder.DropTable(
+                name: "persons");
+        }
+    }
+}
