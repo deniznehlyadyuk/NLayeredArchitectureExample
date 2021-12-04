a2b78610c505ca903ee746380fb0039e8bb65e93
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-04 19:19:56 +0300

    BaseEntity abstract classında yer alan Id propertysi primary key olarak güncellendi.

--- Core/Domain/BaseEntity.cs
+++ Core/Domain/BaseEntity.cs
@@ -1,9 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Core.Domain
 {
     public abstract class BaseEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
     }
 }
