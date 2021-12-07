4661454932f99b9b9c3e3ebcfe2e0ea4419ffc81
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-07 17:04:31 +0300

    Oluşturulan entitylerin dbcontext konfigürasyonu yapıldı.

--- DataAccess/ReservationContext.cs
+++ DataAccess/ReservationContext.cs
@@ -1,3 +1,4 @@
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess
@@ -11,7 +12,92 @@ namespace DataAccess
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Address>(x =>
+            {
+                x.ToTable("addresses");
+            });
+
+            modelBuilder.Entity<Person>(x =>
+            {
+                x.ToTable("persons");
+            });
+            
+            modelBuilder.Entity<Patient>(x =>
+            {
+                x.ToTable("patients");
+                x.HasIndex(y => y.PersonId).IsUnique();
+            });
+
+            modelBuilder.Entity<Employee>(x =>
+            {
+                x.ToTable("employees");
+                x.HasIndex(y => y.PersonId).IsUnique();
+            });
+
+            modelBuilder.Entity<EmployeePermits>(x =>
+            {
+                x.ToTable("employee_permits");
+            });
+
+            modelBuilder.Entity<EmployeeSalary>(x =>
+            {
+                x.ToTable("employee_salaries");
+                x.HasIndex(y => y.EmployeeId).IsUnique();
+            });
+
+            modelBuilder.Entity<Doctor>(x =>
+            {
+                x.ToTable("doctors");
+                x.HasIndex(y => y.EmployeeId).IsUnique();
+            });
+
+            modelBuilder.Entity<Secretary>(x =>
+            {
+                x.ToTable("secretaries");
+                x.HasIndex(y => y.EmployeeId).IsUnique();
+            });
+
+            modelBuilder.Entity<Housekeeper>(x =>
+            {
+                x.ToTable("housekeepers");
+                x.HasIndex(y => y.EmployeeId).IsUnique();
+            });
+            
+            modelBuilder.Entity<SecretaryDoctor>(x =>
+            {
+                x.ToTable("secretary_doctors");
+            });
+
+            modelBuilder.Entity<HousekeeperResponsibleFloor>(x =>
+            {
+                x.ToTable("housekeeper_responsible_floors");
+            });
+
+            modelBuilder.Entity<Reservation>(x =>
+            {
+                x.ToTable("reservations");
+            });
+
+            modelBuilder.Entity<PhysicalExamination>(x =>
+            {
+                x.ToTable("physical_examinations");
+            });
+
             base.OnModelCreating(modelBuilder);
         }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeePermits> EmployeePermits { get; set; }
+        public DbSet<EmployeeSalary> EmployeeSalaries { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Secretary> Secretaries { get; set; }
+        public DbSet<Housekeeper> Housekeepers { get; set; }
+        public DbSet<SecretaryDoctor> SecretaryDoctors { get; set; }
+        public DbSet<HousekeeperResponsibleFloor> HousekeeperResponsibleFloors { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<PhysicalExamination> PhysicalExaminations { get; set; }
     }
 }
