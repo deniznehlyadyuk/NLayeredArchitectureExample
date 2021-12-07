2789c0608235fe7d0349de908d879d361594ac2d
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-07 15:54:46 +0300

    Address entitysi oluşturuldu.

---
+++ Domain/Address.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Core.Domain;
+
+namespace Domain
+{
+    public class Address : BaseEntity
+    {
+        [Required]
+        public string District { get; set; }
+        
+        [Required]
+        public string Neighborhood { get; set; }
+        
+        public string Boulevard { get; set; }
+        public string Street { get; set; }
+        
+        [Required]
+        public int BuildingNo { get; set; }
+        
+        [Required]
+        public int RoomNo { get; set; }
+    }
+}
