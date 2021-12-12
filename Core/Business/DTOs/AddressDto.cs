7340e0f9f5c87ff1dd930dd4f1c98fc7fdeec45b
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-12 11:45:42 +0300

    Adres entitysinin DTO'su oluşturuldu.

---
+++ Core/Business/DTOs/AddressDto.cs
@@ -0,0 +1,12 @@
+namespace Core.Business.DTOs
+{
+    public class AddressDto : IDto
+    {
+        public string District { get; set; }
+        public string Neighborhood { get; set; }
+        public string Boulevard { get; set; }
+        public string Street { get; set; }
+        public int BuildingNo { get; set; }
+        public int RoomNo { get; set; }
+    }
+}
