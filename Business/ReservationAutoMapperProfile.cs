6d2319836121bf55405790d3f9e2f9e2d858ba74
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-12 14:11:23 +0300

    Doctor entitysinin AutoMapper konfigürasyonu yapıldı.

--- Business/ReservationAutoMapperProfile.cs
+++ Business/ReservationAutoMapperProfile.cs
@@ -1,4 +1,7 @@
 using AutoMapper;
+using Core.Business.DTOs;
+using Core.Business.DTOs.Doctor;
+using Domain;
 
 namespace Business
 {
@@ -6,6 +9,25 @@ namespace Business
     {
         public ReservationAutoMapperProfile()
         {
+            CreateMap<AddressDto, Address>();
+            CreateMap<DoctorCreateDto, Person>();
+            CreateMap<DoctorUpdateDto, Person>();
+            CreateMap<DoctorCreateDto, Doctor>();
+            CreateMap<DoctorUpdateDto, Doctor>();
+            CreateMap<Doctor, DoctorGetDto>()
+                .ForMember(x => x.FullName, y => y.MapFrom(z => z.Employee.Person.FullName))
+                .ForMember(x => x.Phone, y => y.MapFrom(z => z.Employee.Person.Phone))
+                .ForMember(x => x.IdentityNumber, y => y.MapFrom(z => z.Employee.Person.IdentityNumber))
+                .ForMember(x => x.Address, y => y.MapFrom(z => new AddressDto
+                {
+                    Boulevard = z.Employee.Address.Boulevard,
+                    District = z.Employee.Address.District,
+                    Neighborhood = z.Employee.Address.Neighborhood,
+                    Street = z.Employee.Address.Street,
+                    BuildingNo = z.Employee.Address.BuildingNo,
+                    RoomNo = z.Employee.Address.RoomNo
+                }));
+
         }
     }
 }
