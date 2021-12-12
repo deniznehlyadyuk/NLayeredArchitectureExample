5c7a728da625cdf438773cc48036f39759107bf0
Deniz Satır <deniznehlyadyuk@gmail.com>
2021-12-12 11:46:19 +0300

    Doctor entitysinin repository'si oluşturuldu.

---
+++ DataAccess/Concrete/DoctorRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.DataAccess;
+using DataAccess.Abstract;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class DoctorRepository : EfEntityRepositoryBase<Doctor, ReservationContext>, IDoctorRepository
+    {
+        public DoctorRepository(ReservationContext context) : base(context)
+        {
+        }
+
+
+        public async Task<Doctor> GetWithInclude(Guid id)
+        {
+            return await DbSet.Where(x => x.Id == id)
+                .Include(x => x.Employee)
+                .ThenInclude(x => x.Person)
+                .Include(x => x.Employee.Address)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
