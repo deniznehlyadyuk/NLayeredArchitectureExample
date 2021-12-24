cd5c5dde3a31c1162f8d3faef4dbef7852d0b2a5
mertdorukkalkan <mertdorukkalkan@gmail.com>
2021-12-24 14:36:05 +0300

    LessonController a CRUD operasyonlarının tamamı eklendi

--- WebAPI/Controllers/LessonController.cs
+++ WebAPI/Controllers/LessonController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Utils;
@@ -23,5 +24,31 @@ namespace WebAPI.Controllers
             var resultEntityDto = await _lessonService.AddAsync(input);
             return Ok(resultEntityDto);
         }
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetLesson(Guid id)
+        {
+            var result = await _lessonService.GetByIdAsync(id);
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllLessons()
+        {
+            var result = await _lessonService.GetAllAsync();
+            return Ok(result);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteLesson(Guid id)
+        {
+            await _lessonService.DeleteByIdAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateLesson(Guid id,LessonDto input)
+        {
+            var result = await _lessonService.UpdateAsync(id, input);
+            return Ok(result);
+        }
+
     }
 }
